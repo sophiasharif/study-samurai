4bb293ca293fb2530776df350bffcfc1855e3d0f
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-03-04 14:03:40 -0800

    basic openai implementation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,59 @@
 <template>
   <div class="hello">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Samurai_with_sword.jpg" alt="">
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Samurai_with_sword.jpg"
+      alt=""
+    />
     <h1>EL STUDY SAMUR.AI</h1>
+    <div>
+      <h2>Calculus Problem:</h2>
+      <p>{{ problem }}</p>
+      <button @click="generateProblem">Generate New Problem</button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+const client = axios.create({
+  headers: {
+    Authorization: "Bearer " + process.env.VUE_APP_OPENAI_API_KEY,
+  },
+});
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      problem: "",
+    };
+  },
+  methods: {
+    async generateProblem() {
+      const params = {
+        prompt:
+          "I don't understand the following problem: Let f(u, v) = tan(uv3). Find the partial derivatives fu(u, v) and fv(u, v). What math concepts is it testing and what am I missing? Give me 3 more practice problems to test my understanding.",
+        model: "text-davinci-003",
+        max_tokens: 1000,
+        temperature: 0.7,
+      };
+
+      try {
+        const result = await client.post(
+          "https://api.openai.com/v1/completions",
+          params
+        );
+        this.problem = result.data.choices[0].text;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
