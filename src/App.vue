3a840956f4a0d6af51034b94db8898d375493f59
juliagu01 <124854549+juliagu01@users.noreply.github.com>
2023-03-05 14:12:11 -0800

    Made working button on home page

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,11 @@
     <router-link to="/check-work">check work</router-link>
     <router-link to="/login" class="button">LOGIN</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+  <div id="smallBlob" class="blob"></div>
+  <div id="mediumBlob" class="blob" width=60px height=80px></div>
+  <div id="largeBlob1" class="blob" width=200px height=240px></div>
+  <div id="largeBlob2" class="blob" width=200px height=240px></div>
 </template>
 
 <style>
@@ -33,5 +37,40 @@
 
   nav a.router-link-exact-active {
     color: var(--lightPurple);
+  }
+
+  .blob {
+    position: absolute; /* specify position! */
+    background-color: white;
+    border-radius: 10px; /* probably will want this to be dynamic */
+    z-index: -1;
+  }
+
+  #smallBlob {
+    left: 120px;
+    top: 130px;
+    width: 45px;
+    height: 50px;
+  }
+
+  #mediumBlob {
+    left: 57%;
+    top: 170px;
+    width: 150px;
+    height: 180px;
+  }
+
+  #largeBlob1 {
+    left: -50px;
+    top: 550px;
+    width: 500px;
+    height: 400px;
+  }
+
+  #largeBlob2 {
+    left: 1050px;
+    top: 650px;
+    width: 600px;
+    height: 500px;
   }
 </style>
