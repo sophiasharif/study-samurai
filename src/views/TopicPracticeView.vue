a5118863bca8ffaa11acb3b3120516cca95577cf
Yyyyt036 <126416001+Yyyyt036@users.noreply.github.com>
2023-03-05 09:41:18 -0800

    Add buttons for unit

--- src/views/TopicPracticeView.vue
+++ src/views/TopicPracticeView.vue
@@ -2,6 +2,10 @@
     <div>
       <h1>Topic Practice</h1>
       <button>Unit one</button>
+      <button>Unit two</button>
+      <button>Unit three</button>
+      <button>Unit four</button>
+      <button>Unit five</button>
     </div>
   </template>
   
