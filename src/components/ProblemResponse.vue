14a343f9bbb7653c1f9a561c6fdfcb54148f4f32
hQian2718 <hqian404@g.ucla.edu>
2023-03-05 13:21:08 -0800

    implemented displaying latex in a paragraph

--- src/components/ProblemResponse.vue
+++ src/components/ProblemResponse.vue
@@ -7,8 +7,15 @@
         <math-jax :latex=formula :block="true"></math-jax>
       </div>
   </div>
+  <div border-style="solid">
+    <template v-for="(item,index) in this.textArray">
+      <span>{{ item }}</span>
+      <math-jax :latex="equationsArray[index]"></math-jax>
+    </template>
+  </div>
 </template>
-  
+
+
 <style>
   .mathjax {
     display: block;
@@ -21,11 +28,22 @@ import { MathJax } from 'mathjax-vue3'
 export default {
   data(){
     return{
-      formula: "\\begin{align} \\int_a^b f(x), dx \\end{align}"
+      formula: "\\begin{align} \\int_a^b f(x), dx \\end{align}",
+      textArray: ['Lorem ipsum ', ' dolor sit amet ', ' consectetur adipiscing elit '],
+      equationsArray: ['\\frac{1}{2}x + 3y = 7', 'e^{i\\pi} + 1 = 0', 'a^2 + b^2 = c^2'],
     };
   },
   components: {
     MathJax
+  },props: {
+    description: {
+      type: String,
+      required: true
+    }
+  },methods: {
+    splitProblem(){
+      blocks = this.description.split('$');
+    }
   }
 }
 </script>
--- src/views/ExamPrepView.vue
+++ src/views/ExamPrepView.vue
@@ -3,14 +3,19 @@
     <h1>Exam Prep</h1>
     <test-topics></test-topics>
     <practice-format-input @generate-test="handleGenerate"></practice-format-input>
-    <problem-response v-if="this.isGenerated" ref="practice"></problem-response>
+    <div class="problemBox">
+      <problem-response></problem-response>
+      <text-math v-if="this.isGenerated" :desc="'Given that $||\\bm{u}|| = 1$, $||\\bm{v}|| = 3$, and $\\bm{u} \\cdot \\bm{v} = 2$, evaluate the expression $2\\bm{u} \\cdot (3\\bm{u} - \\bm{v})$.'"></text-math>
+    </div>
   </div>
 </template>
 
 <script>
-import TestTopics from '../components/TopicSelect.vue'
+import TestTopics from '@/components/TopicSelect.vue'
 import PracticeFormatInput from '@/components/PracticeFormatInput.vue'
 import ProblemResponse from '@/components/ProblemResponse.vue'
+import TextMath from '@/components/TextAndMath.vue';
+
 export default {
   data(){
     return{
@@ -20,7 +25,8 @@ export default {
   components: {
     TestTopics,
     PracticeFormatInput,
-    ProblemResponse
+    ProblemResponse,
+    TextMath
   }, methods:{
     handleGenerate(numProblems, numTime){
       console.log(numTime + "-minute problem set generated with" + numProblems + " problems");
@@ -34,4 +40,8 @@ export default {
 
 <style>
 /* add any custom styles here */
+.problemBox{
+  border-style: solid;
+  border-width:2pt;
+}
 </style>
