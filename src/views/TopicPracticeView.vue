089f6e46d0227e7297c5d12f182e771256303986
Yyyyt036 <126416001+Yyyyt036@users.noreply.github.com>
2023-03-05 16:15:11 -0800

    Update TopicPracticeView.vue

--- src/views/TopicPracticeView.vue
+++ src/views/TopicPracticeView.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
       <h1>Topic Practice</h1>
-      <button>Unit one</button>
-      <button>Unit two</button>
-      <button>Unit three</button>
-      <button>Unit four</button>
-      <button>Unit five</button>
+      <button class="button">Unit one</button>
+      <button class="button">Unit two</button>
+      <button class="button">Unit three</button>
+      <button class="button">Unit four</button>
+      <button class="button">Unit five</button>
+      <button class="button">Add Unit</button>
     </div>
   </template>
   
-  <script></script>
+  <script>
+
+
+</script>
   
   <style scoped></style>
   
