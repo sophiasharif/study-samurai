85e318d4c68cb86792d41df52fe696dada0b1034
hQian2718 <hqian404@g.ucla.edu>
2023-03-05 17:48:17 -0800

    removed not needed options from topic select

--- src/components/TopicSelect.vue
+++ src/components/TopicSelect.vue
@@ -1,35 +1,29 @@
 <template>
-    <div>
-      <div class="dropdown">
-  <button class="dropbtn">Topics</button>
-  <div class="dropdown-content">
-  <a href="" v-for="topic in topics" :key="topic.id" class="topics" :value="topic.name" @click="getSelectedTopics()">{{ topic.name }}</a>
-  </div>
-</div>
-      <select v-model="selectedTopic" @change="handleTopicChange">
-        <option value="">-- Select a Topic --</option>
-        <option v-for="topic in topics" :key="topic.id" class="topics" :value="topic.name">{{ topic.name }}</option>
-        <option value="custom">Custom</option>
-      </select>
-      <div v-if="showCustomInput">
-        <input type="text" v-model="customTopic" placeholder="Enter custom topic...">
-        <button @click="handleCustomTopic">Add</button>
-      </div>
-      <div v-if="selectedTopics.length > 0">
-        <div v-for="selectedTopic in selectedTopics" :key="selectedTopic.id" class="selected-topic">
-          {{ selectedTopic.name }}
-          <button @click="removeSelectedTopic(selectedTopic)">Remove</button>
-        </div>
+  <div>
+    <div class="dropdown">
+      <button class="dropbtn">Topics</button>
+      <div class="dropdown-content">
+        <a href="" v-for="topic in topics" :key="topic.id" class="topics" :value="topic.name" @click="getSelectedTopics()">{{ topic.name }}</a>
       </div>
     </div>
-  </template>
+    <select v-model="selectedTopic" @change="handleTopicChange">
+      <option value="">-- Select a Topic --</option>
+      <option v-for="topic in topics" :key="topic.id" class="topics" :value="topic.name">{{ topic.name }}</option>
+    </select>
+    <div v-if="selectedTopics.length > 0">
+      <div v-for="selectedTopic in selectedTopics" :key="selectedTopic.id" class="selected-topic">
+        {{ selectedTopic.name }}
+        <button @click="removeSelectedTopic(selectedTopic)">Remove</button>
+      </div>
+    </div>
+  </div>
+</template>
   
   <script>
   export default {
     data() {
       return {
         selectedTopic: '',
-        customTopic: '',
         topics: [
           { id: 1, name: 'Linear Algebra' },
           { id: 2, name: 'Analysis' },
@@ -38,27 +32,12 @@
           { id: 5, name: 'Integrability' },
           { id: 6, name: 'Change of Variables' }
         ],
-        showCustomInput: false,
         selectedTopics: []
       }
     },
     methods: {
       handleTopicChange() {
-        if (this.selectedTopic === 'custom') {
-          this.showCustomInput = true
-        } else {
-          this.showCustomInput = false
-          this.addSelectedTopic(this.selectedTopic)
-        }
-      },
-      handleCustomTopic() {
-        if (this.customTopic !== '') {
-          this.topics.push({ id: this.topics.length + 1, name: this.customTopic })
-          this.selectedTopic = this.customTopic
-          this.customTopic = ''
-          this.showCustomInput = false
-          this.addSelectedTopic(this.selectedTopic)
-        }
+        this.addSelectedTopic(this.selectedTopic)
       },
       addSelectedTopic(topic) {
         if (topic !== '' && !this.selectedTopics.find(t => t.name === topic)) {
