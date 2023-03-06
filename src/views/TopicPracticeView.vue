0b65cd5c68233c5e0ef730a061a3e44cfb53d0e0
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-03-05 18:12:40 -0800

    generate random question

--- src/views/TopicPracticeView.vue
+++ src/views/TopicPracticeView.vue
@@ -1,35 +1,53 @@
 <template>
   <div>
     <h1>Topic Practice</h1>
+    <select name="Topics" id="button">
+      <option value="">-- Select a Topic --</option>
+      <option
+        v-for="topic in topic"
+        :key="topic.id"
+        class="topics"
+        :value="topic.name"
+      >
+        {{ topic.name }}
+      </option>
+      <option value="custom">Custom</option>
+    </select>
+    <button @click="generateQuestion">Generate Question</button>
     <div v-if="questionLoaded">
       <Suspense>
         <MultipleChoiceQuestion :questionObject="question" />
-        <template #fallback> Loading suspended component... </template>
+        <template #fallback> Generating a practice question... </template>
       </Suspense>
     </div>
-    <div v-else>QUESTION LOADING</div>
   </div>
-  <select name="Topics" id="button">
-        <option value="">-- Select a Topic --</option>
-        <option v-for="topic in topic" :key="topic.id" class="topics" :value="topic.name">{{ topic.name }}</option>
-        <option value="custom">Custom</option>
-        </select>
 </template>
 
 <script setup>
-
-      
 import MultipleChoiceQuestion from "@/components/MultipleChoiceQuestion.vue";
-import { getQuestionsBySubtopic } from "@/composables/firebaseFunctions";
+import { getQuestionsBySubtopic, getQuestionsByUnit } from "@/composables/firebaseFunctions";
 import { ref } from "vue";
-import topic from '@/components/TopicSelect.vue'
 
 const question = ref({});
 const questionLoaded = ref(false);
-getQuestionsBySubtopic("orthogonality").then((res) => {
-  question.value = res[0];
-  questionLoaded.value = true;
-});
+const unit = ref("1");
+const subunit = ref(null);
+
+function generateQuestion() {
+  if (subunit.value) {
+    getQuestionsBySubtopic(subunit.value).then((res) => {
+      const index = Math.floor(Math.random()*res.length)
+      question.value = res[index];
+      questionLoaded.value = true;
+    });
+  } else if (unit.value) {
+    getQuestionsByUnit(Number(unit.value)).then((res) => {
+      const index = Math.floor(Math.random()*res.length)
+      question.value = res[index];
+      questionLoaded.value = true;
+    });
+  }
+}
 </script>
 
 <style scoped></style>
